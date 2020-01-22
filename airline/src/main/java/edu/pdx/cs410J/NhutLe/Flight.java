d03f0e180dfdd8dcbaea221458f1c85433b4f841
letatanu <letatanu@gmail.com>
2020-01-22 11:19:14 -0800

    Done

--- airline/src/main/java/edu/pdx/cs410J/NhutLe/Flight.java
+++ airline/src/main/java/edu/pdx/cs410J/NhutLe/Flight.java
@@ -2,29 +2,69 @@ package edu.pdx.cs410J.NhutLe;
 
 import edu.pdx.cs410J.AbstractFlight;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Flight extends AbstractFlight {
+  private final int flightNumber;
+  private final String src;
+  private final String depart;
+  private final String dest;
+  private final String arrive;
+
+  public Flight() {
+    super();
+    this.flightNumber = 42;
+    this.src = "";
+    this.dest = "";
+    this.depart = "";
+    this.arrive = "";
+  }
+  private void ValidateCode(String code) throws IllegalArgumentException {
+    if (code.length() != 3 ) {
+      throw new IllegalArgumentException(src + " is not 3-letter code");
+    }
+    Pattern codeFormat = Pattern.compile("[a-zA-Z]");
+    Matcher isLetter = codeFormat.matcher(code);
+    if (!isLetter.find()) {
+      throw  new IllegalArgumentException(src + " contains numbers or special characters");
+    }
+  }
+  public Flight(int flightNumber, String src, String depart, String dest, String arrive ) {
+    super();
+    this.flightNumber = flightNumber;
+
+    ValidateCode(src);
+    ValidateCode(dest);
+
+    this.src = src;
+    this.depart = depart;
+    this.dest = dest;
+    this.arrive = arrive;
+  }
+
   @Override
   public int getNumber() {
-    return 42;
+    return this.flightNumber;
   }
 
   @Override
   public String getSource() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+   return this.src;
   }
 
   @Override
   public String getDepartureString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return this.depart;
   }
 
   @Override
   public String getDestination() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return this.dest;
   }
 
   @Override
   public String getArrivalString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return this.arrive;
   }
 }
--- airline/src/main/java/edu/pdx/cs410J/NhutLe/Project1.java
+++ airline/src/main/java/edu/pdx/cs410J/NhutLe/Project1.java
@@ -1,19 +1,84 @@
 package edu.pdx.cs410J.NhutLe;
 
-import edu.pdx.cs410J.AbstractAirline;
+import edu.pdx.cs410J.AbstractFlight;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeParseException;
 
 /**
  * The main class for the CS410J airline Project
  */
 public class Project1 {
-
   public static void main(String[] args) {
-    Flight flight = new Flight();  // Refer to one of Dave's classes so that we can be sure it is on the classpath
-    System.err.println("Missing command line arguments");
+
+    /* Check if the option <code>-README</code> is input.
+    * If yes, print out <code> README </code> and exit.
+    */
     for (String arg : args) {
-      System.out.println(arg);
+      if (arg.charAt(0) == '-') {
+        String option = arg.substring(1);
+        if (option.equals("README")) {
+          String Readme = "Readme";
+          System.out.println(Readme);
+          System.exit(1);
+        }
+      }
     }
+
+    // If there's no option <code>README</code>. Check the number of arguments input.
+    if (args.length < 6) {
+      System.err.println("Missing command line arguments");
+      System.exit(1);
+    }
+
+    // Getting airline name from the argument 0
+    String airlineName = args[0];
+
+    // try to parse <code>flightNumber<code> from the argument 1 to integer.
+    try {
+      // Getting flight number from the argument 1
+      int flightNumber = Integer.parseInt(args[1]);
+
+      // Getting <code>source</code>
+      String src = args[2];
+
+      // Getting <code>depart</code>
+      String depart = args[3];
+
+      // Getting <code>destination</code>
+      String dest = args[4];
+
+      // Getting <code>arrival</code>
+      String arrive = args[5];
+
+      // Create a new flight from the input
+      Flight flight = new Flight(flightNumber, src, depart, dest, arrive);
+
+      // create an airline and add the flight to the airline
+      Airline airline = new Airline(airlineName);
+      airline.addFlight(flight);
+
+      printOption(args,airline);
+
+    } catch (NumberFormatException e) {
+      System.err.println("The flight number must be an integer");
+      System.exit(1);
+    }
+
     System.exit(1);
   }
 
+  private static void printOption(String[] args, Airline airline) {
+    for (String arg : args) {
+      if (arg.charAt(0) == '-') {
+        String option = arg.substring(1);
+        if (option.equals("print")) {
+          System.out.println("Airline: " + airline.getName());
+          for (AbstractFlight flight : airline.getFlights()) {
+            System.out.println(flight.toString());
+          }
+        }
+      }
+    }
+
+  }
 }
